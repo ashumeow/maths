b0d91433f3b1c9d7fb5e97a9286b8f5a9c2cb485
Aswini S <ashumeow@live.com>
2014-02-20 10:57:39 +0530

    Create arithop1.fs

---
+++ arithop1.fs
@@ -0,0 +1,13 @@
+// F# Program for developing mathematical operations
+module arithop1
+open System
+open System.Collections.Generic
+
+[<Category("Arithmetic");
+Title("Arithmetic Operations");
+Description("This is an arithmetic operations program")>]
+let arithop()=
+let x=5+3+2
+let y=2*3+5-1
+let w=x%5
+printfn "w=%d,x=%d,y=%d,z=%d" w x y z
